90f7ba1754d4348694e410c64221bc2ee61fb7d2
allan-bit-web <allankinuthia59@gmail.com>
2025-07-17 12:06:16 +0300

    changed the db file to remove error in render

--- server/config/db.js
+++ server/config/db.js
@@ -2,10 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect(process.env.MONGO_URI
+        //     , {
+        //     useNewUrlParser: true,
+        //     useUnifiedTopology: true
+        // }
+    );
         console.log("MongoDB Connected successfully!!!!!");
     } catch (error) {
         console.error("MongoDB connection Error", error.message);
